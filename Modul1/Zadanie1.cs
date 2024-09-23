4ae4aae9ec6b58ca8e67092c160d8fbc1f91b542
GGWP650 <uniloft650@gmail.com>
2024-09-23 11:26:47 +0300

    Update Zadanie1.cs

--- Modul1/Zadanie1.cs
+++ Modul1/Zadanie1.cs
@@ -4,9 +4,11 @@ class Zadanie1
 {
     static void Main()
     {
+        // Чтение введенной строки с консоли, преобразование её в целое число и сохранение в переменную number
         Console.WriteLine("Введите число:");
         int number = Convert.ToInt32(Console.ReadLine());
 
+        // Проверка, делится ли число на 2 без остатка
         if (number % 2 == 0)
         {
             Console.WriteLine("Число четное.");
