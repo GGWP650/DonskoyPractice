909902806d3b1c7d9197304d80526453580c7edf
GGWP650 <uniloft650@gmail.com>
2024-10-07 12:13:10 +0300

    Add files via upload

---
+++ Modul2/StructStudent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+public struct Student
+{
+    public string lastName;
+    public string initials;
+    public int groupNumber;
+    public int[] grades;
+
+    // Метод для расчета среднего балла
+    public double AverageGrade()
+    {
+        return grades.Average();
+    }
+
+    // Метод для проверки, есть ли только оценки 4 и 5
+    public bool HasOnlyHighGrades()
+    {
+        return grades.All(g => g >= 4);
+    }
+}
+
+class Students
+{
+    static void Run(string[] args)
+    {
+        // Массив студентов
+        Student[] students = new Student[3]
+        {
+            new Student { lastName = "Иванов", initials = "А.И.", groupNumber = 101, grades = new int[] { 4, 5, 4, 4, 5 } },
+            new Student { lastName = "Петров", initials = "Б.В.", groupNumber = 102, grades = new int[] { 3, 4, 5, 3, 4 } },
+            new Student { lastName = "Сидоров", initials = "В.Г.", groupNumber = 101, grades = new int[] { 5, 5, 5, 5, 5 } },
+            
+        };
+
+        // Сортировка по возрастанию среднего балла
+        var sortedStudents = students.OrderBy(s => s.AverageGrade()).ToArray();
+
+        // Вывод отсортированных студентов
+        Console.WriteLine("Студенты, отсортированные по среднему баллу:");
+        foreach (var student in sortedStudents)
+        {
+            Console.WriteLine($"{student.lastName}, Группа: {student.groupNumber}, Средний балл: {student.AverageGrade():F2}");
+        }
+
+        // Вывод студентов с оценками 4 и 5
+        Console.WriteLine("\nСтуденты с оценками 4 и 5:");
+        foreach (var student in students)
+        {
+            if (student.HasOnlyHighGrades())
+            {
+                Console.WriteLine($"{student.lastName}, Группа: {student.groupNumber}");
+            }
+        }
+    }
+}
---
+++ Modul2/Zadanie1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class Person
+{
+    // Поля класса
+    private string name;
+    private int age;
+    private string address;
+
+    // Конструктор класса
+    public Person(string name, int age, string address)
+    {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Методы для установки значений
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetAge(int age)
+    {
+        if (age >= 0)
+        {
+            this.age = age;
+        }
+        else
+        {
+            Console.WriteLine("Возраст не может быть отрицательным.");
+        }
+    }
+
+    public void SetAddress(string address)
+    {
+        this.address = address;
+    }
+
+    // Методы для получения значений
+    public string GetName()
+    {
+        return name;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public string GetAddress()
+    {
+        return address;
+    }
+
+    // Метод для вывода информации о человеке
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Имя: {name}, Возраст: {age}, Адрес: {address}");
+    }
+}
+
+class Zadanie1
+{
+    static void Run(string[] args)
+    {
+        // Создание объектов класса Person
+        Person person1 = new Person("Алексей", 30, "Москва, ул. Ленина, д.1");
+        Person person2 = new Person("Мария", 25, "Санкт-Петербург, ул. Пушкина, д.100");
+
+        // Вывод информации о людях
+        person1.PrintInfo();
+        person2.PrintInfo();
+    }
+}
